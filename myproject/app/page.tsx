6520cc1bc249d62401aaa9933428b30ebb450623
Irfnmdlptra <bangasrat@gmail.com>
2023-06-20 15:57:41 +0800

    asa

--- myproject/app/page.tsx
+++ myproject/app/page.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 export default function Home() {
   const [state, setState] = useState(0);
 
-  function
+  function tambah() {
+    setState(state + 1);
+  }
   return <h1>helloworld!</h1>;
 }
