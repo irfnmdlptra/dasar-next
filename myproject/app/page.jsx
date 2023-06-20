19e85d2bcfe1fb0e743cf6e74b26560d887c6364
Irfnmdlptra <bangasrat@gmail.com>
2023-06-20 16:50:57 +0800

    asa

--- myproject/app/page.jsx
+++ myproject/app/page.jsx
@@ -1,14 +1,14 @@
 function Button(props) {
-    function text() {
-        return <p>saya</p>
-    }
-    return <button onClick={text}>{props.desc}</button>
+  function text() {
+    return <p>saya</p>;
+  }
+  return <button onClick={text}>{props.desc}</button>;
 }
 export default function Home() {
-    return (
-        <>
-        <Button  desc="NAMA"/>
-        <Button desc="NAMA"/>
-        </>
-    )
+  return (
+    <>
+      <Button desc="NAMA" />
+      <Button desc="NAMA" />
+    </>
+  );
 }
