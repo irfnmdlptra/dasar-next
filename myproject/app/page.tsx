9397eb4f1d5387b7c88bb98bfb6ee8a296103c43
Irfnmdlptra <bangasrat@gmail.com>
2023-06-20 16:06:03 +0800

    as

--- myproject/app/page.tsx
+++ myproject/app/page.tsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function Home() {
-  const  [state, setState] = useState(0);
+  const [state, setState] = useState(0);
 
   function tambah() {
     setState(state + 1);
   }
-  return (<>
 
-<h1>{state}!</h1>;
-<button onClick={tambah}></button>
-
-  </>
-  )
-
+  return (
+    <>
+      <h1>{state}!</h1>
+      <button onClick={tambah}>Increment</button>
+    </>
+  );
 }
