b9e5cba537709d76eed10e0477281dee66ae285f
Irfnmdlptra <bangasrat@gmail.com>
2023-06-21 08:36:10 +0800

    sad

--- myproject/app/page.jsx
+++ myproject/app/page.jsx
@@ -1,17 +1,3 @@
-import { useState } from "react";
-
-function Button(props) {
-    const [text, setText] = useState(0)
-  function stext() {
-    setText(text + 1);
-  }
-  return <button onClick={stext}>{props.desc{text}</button>;
-}
 export default function Home() {
-  return (
-    <>
-      <Button desc="makan"/>
-      <Button desc="minum"/>
-    </>
-  );
+  return <div>Halaman Utama</div>
 }
