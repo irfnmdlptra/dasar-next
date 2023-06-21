7a79bf6c22b0576acae59b25823e3a58feac6c8d
Irfnmdlptra <bangasrat@gmail.com>
2023-06-21 09:08:41 +0800

    add

--- myproject/app/page.tsx
+++ myproject/app/page.tsx
@@ -3,7 +3,8 @@ export default function Home() {
     <ul>
       <li>Home</li>
       <li>Dashboard</li>
-      <li>Home</li>
+      <li>Setiings</li>
     </ul>
+    <p>list di atas adalah nama halaman yang ada di dalam folder app</p>
   </div>
 }
