28f4a3c1005ca9e0488107320d21646be7cdc23c
Irfnmdlptra <bangasrat@gmail.com>
2023-06-21 09:20:00 +0800

    asa

--- myproject/app/page.tsx
+++ myproject/app/page.tsx
@@ -1,12 +1,7 @@
-
 export default function Home() {
   return (
     <div>
-      <ul>
-        <li><Link href="/">Home</Link></li>
-        <li>Dashboard</li>
-        <li>Setiings</li>
-      </ul>
+     
       <p>list di atas adalah nama halaman yang ada di dalam folder app</p>
     </div>
   );
