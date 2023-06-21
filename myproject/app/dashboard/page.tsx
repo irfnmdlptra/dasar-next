8b092115bc530f9e607003fa9f9ff563163220cc
Irfnmdlptra <bangasrat@gmail.com>
2023-06-21 09:12:43 +0800

    asas

--- myproject/app/dashboard/page.tsx
+++ myproject/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
 import Section from "./section";
+import "../../public/src/styles/global/css";
 
 export default function Dashboard() {
-    return <div>
-        <div>di bawah ini adalah tombol section button</div>
-        <Section/>
+  return (
+    <div>
+      <div>di bawah ini adalah tombol section button</div>
+      <Section />
     </div>
+  );
 }
