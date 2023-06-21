3a77fa7c8fdfaff76837d4ea22576da844fffe46
Irfnmdlptra <bangasrat@gmail.com>
2023-06-21 09:24:40 +0800

    as

--- myproject/app/settings/layout.tsx
+++ myproject/app/settings/layout.tsx
@@ -1,9 +1,12 @@
-export default function layout({children}:{children: React.ReactNode}) {
-    return <div>
-        <p>ini adalah halaman layout khusus settings</p>
-        {children}
-        <button>
-            jangan di klik!
-        </button>
+import Link from "next/link";
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <p>ini adalah halaman layout khusus settings</p>
+      {children}
+      <button>
+        <Link href="/">jangan di klik!</Link>
+      </button>
     </div>
+  );
 }
