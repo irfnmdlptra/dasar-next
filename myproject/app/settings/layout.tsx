39347ccce95d3693b35935d4980f4c0834107674
Irfnmdlptra <bangasrat@gmail.com>
2023-06-21 08:54:22 +0800

    asa

--- myproject/app/settings/layout.tsx
+++ myproject/app/settings/layout.tsx
@@ -1,3 +1,9 @@
 export default function layout({children}:{children: React.ReactNode}) {
-
+    return <div>
+        <p>ini adalah halaman layout khusus settings</p>
+        {children}
+        <button>
+            jangan di klik!
+        </button>
+    </div>
 }
