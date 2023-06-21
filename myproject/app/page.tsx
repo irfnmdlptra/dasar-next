a37ef5341176f6dcc32ee8096257a27838a827dc
Irfnmdlptra <bangasrat@gmail.com>
2023-06-21 09:15:11 +0800

    add

--- myproject/app/page.tsx
+++ myproject/app/page.tsx
@@ -1,13 +1,14 @@
-import "../public/src/styles/global.css";
+import "../public/src/styles/global.";
 
 export default function Home() {
-
-  return <div>
-    <ul>
-      <li>Home</li>
-      <li>Dashboard</li>
-      <li>Setiings</li>
-    </ul>
-    <p>list di atas adalah nama halaman yang ada di dalam folder app</p>
-  </div>
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Dashboard</li>
+        <li>Setiings</li>
+      </ul>
+      <p>list di atas adalah nama halaman yang ada di dalam folder app</p>
+    </div>
+  );
 }
